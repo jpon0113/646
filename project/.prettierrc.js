b7dfbbc0cd55a94203de5de2367a542dbfedd282
jpon0113 <jpongama@gamil.com>
2023-05-15 07:03:30 +0200

    feat: react 專案 init

---
+++ project/.prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // 箭頭函數只有一個參數時候可以忽略括號
+  arrowParens: "avoid",
+  // 括號內部不要出現空格
+  bracketSpacing: true,
+  // 行結束符使用Unix格式
+  endOfLine: "lf",
+  //
+  jsxBracketSameLine: false,
+  // 行寬
+  printWidth: 100,
+  // 換行方式
+  proseWrap: "preserve",
+  // 分號
+  semi: false,
+  // 使用單引號
+  singleQuote: true,
+  // 縮進
+  tabWidth: 2,
+  // 使用tab縮進
+  useTabs: false,
+  // 後置逗號, 多行對象, array最後一行新增逗號
+  trailingComma: "es5",
+  parser: "typescript",
+}
